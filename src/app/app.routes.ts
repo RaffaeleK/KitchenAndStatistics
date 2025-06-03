0bbb601074e4a4ba5fda9c7e0ca83a919c991751
ZhangRenw <renwei.zhang.stud@ispascalcomandini.it>
2025-06-03 13:40:32 +0200

    aggiunta routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { KitchenComponent } from './components/kitchen/kitchen.component';
+import { StatisticsComponent } from './components/statistics/statistics.component';
+import { LoginComponent } from './components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'kitchen', component:KitchenComponent},
+    {path: 'stats', component: StatisticsComponent},
+    {path: '**', component: LoginComponent}
+];
