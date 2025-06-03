8c47d0e03bd7c74170c186cfd5267e645829ad77
RaffaeleK <zannraff@gmail.com>
2025-06-03 13:15:13 +0200

    aggiunta model

---
+++ src/app/model/order.ts
@@ -0,0 +1,11 @@
+export class Order
+{
+    id?: number
+    productId?: number
+    tableId?: number
+    name?: string
+    qty?: number
+    price?: number
+    orderDate?: Date
+    completionDate?: Date
+}
