5482dcaac716155ed02dce808ce142920c3dc304
ZhangRenw <renwei.zhang.stud@ispascalcomandini.it>
2025-06-04 10:09:46 +0200

    Aggiunta authGuards differenti e eliminazione di quello unico

--- src/app/components/guards/authK.guards.ts
+++ src/app/components/guards/authK.guards.ts
@@ -2,7 +2,7 @@ import { inject } from "@angular/core";
 import { CanActivateFn, Router } from "@angular/router";
 import { AuthService } from "../../services/auth.service";
 
-export const AuthGuard: CanActivateFn = (route, state) => {  
+export const AuthGuardK: CanActivateFn = (route, state) => {  
   const authService = inject(AuthService);
   const router = inject(Router);
   if(!authService.isLogged())
@@ -10,5 +10,12 @@ export const AuthGuard: CanActivateFn = (route, state) => {
       router.navigate(['login']);
       return false;
   }
+
+
+  
+  if(authService.getFieldFromToken() == "stats"){
+    router.navigate(['login']);
+  }
+
   return true;
 };
