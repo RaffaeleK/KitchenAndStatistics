6bf8cd83dabbf37cb5831a1dd2ce114fb50625c5
ZhangRenw <renwei.zhang.stud@ispascalcomandini.it>
2025-06-05 10:56:13 +0200

    stats semi completato

--- src/app/components/statistics/statistics.component.ts
+++ src/app/components/statistics/statistics.component.ts
@@ -1,19 +1,107 @@
 import { Component } from '@angular/core';
 import { StatsService } from '../../services/stats.service';
+import { Table } from '../../model/table';
+import { OrderStats } from '../../model/order';
+import { environment } from '../../../enviroments/enviroment';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-statistics',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './statistics.component.html',
   styleUrl: './statistics.component.css'
 })
 export class StatisticsComponent {
-  
+  tables: Table[] = [];
+  orders: OrderStats[] = [];
+
+  mostOrdered: string = "";
+
+  requestedOrders: Map<string, number> = new Map<string, number>();
+
   constructor(private statsService: StatsService) { 
+    statsService.getTables().subscribe(r => this.tables = r)
+    statsService.getOrdersStats().subscribe(r => {this.orders = r; this.organiseOrdersByPeople()});
     
   }
   
-  getAllOccupetedTable(){
-    this.statsService.getTables().subscribe()
+  getAllOccupietedTable(): number{
+    return this.tables.filter(t => t.occupied).length;
+  }
+
+  getNumberOfCustomers(): number{
+    let numOfCust = 0;
+
+    this.tables.forEach(t => {
+      if(t.occupied)
+        numOfCust += t.occupants!;
+    })
+
+    return numOfCust;
+  }
+
+  getWaitingOrders(): number{
+    let waitingOrders = 0;
+
+    this.orders.forEach( o =>{
+      if(o.completionDate == null)
+        waitingOrders++;
+    })
+
+    return waitingOrders;
+  }
+
+  
+  getPreparedOrders(): number{
+    let preparedOrders = 0;
+
+    this.orders.forEach( o =>{
+      if(o.completionDate != null)
+        preparedOrders++;
+    })
+
+    return preparedOrders;
+  }
+
+  getMostOrderedDish(): string  {
+    this.mostOrdered = this.requestedOrders.keys().next().value!;
+    return environment.apiImage + this.mostOrdered.toLowerCase().replace(" ", "_") + ".jpeg";
+  }
+
+  getFirstFiveOrderedDish(): string[] {
+    let orderedDishes: string[] = [];
+    let count = 0;
+
+    this.requestedOrders.forEach((value, key) => {
+      if (count < 5) {
+        console.log(environment.apiImage + this.orders.find(o => o.name == key)!.image!)
+        orderedDishes.push(key.toLowerCase().slice(key.indexOf(` (`), key.length - key.indexOf(' (')).replace(' ', '_'));
+        count++;
+      }
+    });
+
+    return orderedDishes;
+  }
+
+  findDish(name: string): string{
+    console.log(environment.apiImage + this.orders.find(o => o.name == name)!.image!)
+    return environment.apiImage + this.orders.find(o => o.name == name)!.image!;
+  }
+
+  organiseOrdersByPeople(): Map<string, number> {   
+
+    this.orders.forEach(order => {
+      if (this.requestedOrders.has(order.name!)) {
+        this.requestedOrders.set(order.name!, order.qty! + this.requestedOrders.get(order.name!)!);
+      } else {
+        this.requestedOrders.set(order.name!, order.qty!)
+      }
+    });
+    
+    return this.requestedOrders;
+  }
+
+  getApi(): string{
+    return environment.apiImage;
   }
 }
